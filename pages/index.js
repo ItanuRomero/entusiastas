7464cbf5681bc3af44c6a323ac7c4e681dc83e0a
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2023-03-09 22:50:22 +0000

    feat: create a love message to someone that I care about

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+    return (
+    <div>
+        <h1>Oi, Marayita (meu amor)</h1>
+        <p>
+            Você é uma pessoa incrível que eu tenho o prazer de ajudar a crescer.
+            Obrigado por isso e por tudo que já vivemos.
+            <br></br><br></br>
+            Te amo mucho &lt;3
+            <br></br><br></br>
+            Do seu, <br></br><br></br>
+            Itanú Romero
+        </p>
+    </div>
+    )
+}
+
+export default Home;
