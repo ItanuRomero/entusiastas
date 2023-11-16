286d7f1c64b2520eaea0235e0b052a36b3bbbe0e
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2023-11-16 21:19:53 +0000

    feature(building-page): create base page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
 function Home() {
   return (
     <>
-      <h1>Hello World Again</h1>
-      <p>testando o lint on save</p>
+      <section
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+          height: "100vh",
+        }}
+      >
+        <div>
+          <h1>
+            Escreva sobre o que você é <strong>entusiasta</strong>
+          </h1>
+          <p>Ainda estamos trabalhando nisso, mas não esqueça de voltar</p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            style={{ borderRadius: "9999px", width: "200px" }}
+            src="https://avatars.githubusercontent.com/u/66224956?v=4"
+          />
+          <p>
+            Sendo desenvolvido por:{" "}
+            <a href="https://github.com/ItanuRomero">Itanú Romero</a>
+          </p>
+        </div>
+      </section>
     </>
   );
 }
