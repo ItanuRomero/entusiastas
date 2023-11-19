ed619cd9a3eea1387a890d55ee9ca21dfceb1429
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2023-11-19 16:27:32 +0000

    feat(tests): delete tests that will be not used

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(numero1, numero2) {
-  if (typeof numero1 !== "number" || typeof numero2 !== "number") {
-    return "Erro";
-  }
-  return numero1 + numero2;
-}
-
-exports.somar = somar;
