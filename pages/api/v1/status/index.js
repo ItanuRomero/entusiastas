5a7d6754f48604e42f185ccb88946468f31686b6
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2023-11-26 19:43:03 +0000

    feat(env): create `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
