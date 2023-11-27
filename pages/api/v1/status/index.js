58b9f1dd5b0c81d0ce05eccb90af28876def857c
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2023-11-27 00:08:21 +0000

    feat(onfig): add jsconfig.json and cset baseUrl

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 export default async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
