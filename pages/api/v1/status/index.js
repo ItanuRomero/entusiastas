f907a97b28fe9e007ca758512c9b26b712f101f4
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2023-12-05 23:04:57 +0000

    refac(status): add ; and change name

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,17 +2,17 @@ import database from "infra/database.js";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const databaseVersion = (await database.query("SHOW SERVER_VERSION")).rows[0]
+  const databaseVersion = (await database.query("SHOW SERVER_VERSION;")).rows[0]
     .server_version;
-  const databaseMaxConnections = (await database.query("SHOW MAX_CONNECTIONS"))
+  const databaseMaxConnections = (await database.query("SHOW MAX_CONNECTIONS;"))
     .rows[0].max_connections;
   const databaseUsedConnections = (
-    await database.query("SELECT count(*) FROM pg_stat_activity")
+    await database.query("SELECT count(*) FROM pg_stat_activity;")
   ).rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
-    services: {
+    dependencies: {
       database: {
         version: databaseVersion,
         max_connections: Number(databaseMaxConnections),
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toBe(parsedUpdatedAt);
 
-  const database = responseBody.services.database;
+  const database = responseBody.dependencies.database;
   expect(database).toBeDefined();
 
   expect(database.version).toBe("16.0");
