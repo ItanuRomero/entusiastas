e143264db3a0a3632efbbbd002d8db8f7cc31e08
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2023-12-05 23:23:21 +0000

    fix(status): handle 500 error when cannot connect database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,22 +2,26 @@ import database from "infra/database.js";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const databaseVersion = (await database.query("SHOW SERVER_VERSION;")).rows[0]
-    .server_version;
-  const databaseMaxConnections = (await database.query("SHOW MAX_CONNECTIONS;"))
-    .rows[0].max_connections;
-  const databaseUsedConnections = (
-    await database.query("SELECT count(*) FROM pg_stat_activity;")
-  ).rows[0].count;
+  const databaseInfo = {};
+  try {
+    databaseInfo.version = (
+      await database.query("SHOW SERVER_VERSION;")
+    ).rows[0].server_version;
+    databaseInfo.max_connections = Number(
+      (await database.query("SHOW MAX_CONNECTIONS;")).rows[0].max_connections,
+    );
+    databaseInfo.used_connections = Number(
+      (await database.query("SELECT count(*) FROM pg_stat_activity;")).rows[0]
+        .count,
+    );
+  } catch (e) {
+    if (e instanceof Error) databaseInfo.error = "Cannot connect with database";
+  }
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
-      database: {
-        version: databaseVersion,
-        max_connections: Number(databaseMaxConnections),
-        used_connections: Number(databaseUsedConnections),
-      },
+      database: { ...databaseInfo },
     },
   });
 }
