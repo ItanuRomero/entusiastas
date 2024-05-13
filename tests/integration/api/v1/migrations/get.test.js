077b36bd77df681df6fe360ab9a0bde4d4a01868
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2024-05-13 22:57:57 +0000

    feat(migrations): add endpoint `/migrations`

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,10 @@
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  console.log(responseBody);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+});
