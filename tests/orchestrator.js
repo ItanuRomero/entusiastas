04d64122e76b30fb778272cf588dcacc1e055587
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2024-07-09 20:41:31 +0000

    feat(scripts): adjust scripts to be more performatic

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,6 +6,7 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
