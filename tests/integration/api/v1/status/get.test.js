86f7d9420297ed30bece267cbbc1d8e0a7071424
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2024-09-10 10:11:23 +0000

    test: rename all descriptions to new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,24 +1,28 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toBe(parsedUpdatedAt);
 
-  const database = responseBody.dependencies.database;
-  expect(database).toBeDefined();
+      const database = responseBody.dependencies.database;
+      expect(database).toBeDefined();
 
-  expect(database.version).toBe("16.0");
+      expect(database.version).toBe("16.0");
 
-  expect(database.max_connections).toEqual(100);
+      expect(database.max_connections).toEqual(100);
 
-  expect(database.opened_connections).toEqual(1);
+      expect(database.opened_connections).toEqual(1);
 
-  expect(Object.keys(database)).toEqual([
-    "version",
-    "max_connections",
-    "opened_connections",
-  ]);
+      expect(Object.keys(database)).toEqual([
+        "version",
+        "max_connections",
+        "opened_connections",
+      ]);
+    });
+  });
 });
