82ef5e41c0a512f41b269f11128ea4bbca2be6c8
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2024-09-14 19:04:01 +0000

    fix(migrations): change `join` to `resolve` to solve vercel path resolving

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
@@ -19,7 +19,7 @@ export default async function migrations(request, response) {
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: join("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
